10b97a35df97c99cfde1cd3bc2273566b6e263c5
pablotori98 <pablotori98@gmail.com>
2022-10-24 18:50:52 +0000

    añadimos card modificado y funcionando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,9 +10,23 @@ const Home = () => {
 			<Navbar />
 
 			<Jumbotron />
+			<div className="d-flex m-4">
+				<Card img="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/10/24/16665852848983.jpg"
+				textTitle= "Fernando Alonso"
+				/>
 
-			<Card />
+				<Card img="https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2022/02/25/16458014237280.jpg" 
+				textTitle= "Carlos Sainz"
+				/>
 
+				<Card img="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/10/23/16665585672551.jpg" 
+				textTitle= "Max Verstappen"
+				/>
+
+				<Card img="https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2022/08/28/16616934513327.jpg" 
+				textTitle= "Hamilton and Alonso"
+				/>
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,6 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-import Navbar from "./component/navbar.jsx"
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
